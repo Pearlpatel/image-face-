49845b801c8cda534c113db6c5aeb31c090a5b99
Pearlpatel <81533030+Pearlpatel@users.noreply.github.com>
2021-09-25 11:24:59 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+Webcam.set({
+width:350,
+height:300,
+image_format:"png",
+png_quality:90
+});
+
+get_div=document.getElementById("camera");
+
+Webcam.attach("#camera");
+
+function take_pic() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML="<img id='img_show' src='"+data_uri+"'/>";
+    });
+}
+
+console.log("The version of ml5: ", ml5.version);
+
+what_model=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/_mM75blbM/model.json",modelloaded);
+function modelloaded() {
+    console.log("model is loaded");
+
+}
